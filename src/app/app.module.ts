f29ece2727891e3790b379e71a4ccadf625b94b9
Siddhesh Thadeshwar <djsiddz@gmail.com>
2022-07-13 22:38:26 +0530

    Add Angular Material with a preset theme, typography, animations and CDK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NotesComponent } from './notes/notes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { NotesComponent } from './notes/notes.component';
     NotesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
